0f199b5dcabb837b35a19fc165821cdec5d1cc6d
janejanejane <jeanclaudetteambait@gmail.com>
2012-10-11 22:18:03 +0800

    Fix search for category controller.

--- app/controllers/category_controller.rb
+++ app/controllers/category_controller.rb
@@ -1,4 +1,6 @@
 class CategoryController < ApplicationController
+  before_filter :search
+
 	def show
 		@category = params[:id]
     @jobs = Job.confirmed.find_all_by_category(@category)
@@ -6,4 +8,20 @@ class CategoryController < ApplicationController
     	render 'static_pages/not_found'
     end
   end
+
+	private
+	  def search
+	    @word = params[:search]
+	    @search = 0
+	    if !@word.nil?
+	      if @word != "" || !@word.blank?
+	        @query = "%" + @word + "%"
+	        @results = Job.where(['lower(jobtitle) LIKE ? OR lower(description) LIKE ? OR
+	                              lower(category) LIKE ? OR lower(company_name) LIKE ? OR
+	                              lower(location) LIKE ? ', @query, @query, @query, @query, @query]).group_by { |job| job.category }
+	        @search = @results.length
+	      end
+	      render 'static_pages/search'
+	    end
+  	end
 end
