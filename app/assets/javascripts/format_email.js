4861bc56a08a26eda49831be0a0014c596f38c5d
janejanejane <jeanclaudetteambait@gmail.com>
2012-11-25 13:09:07 +0800

    Fix email link in Apply Details section

---
+++ app/assets/javascripts/format_email.js
@@ -0,0 +1,19 @@
+$('document').ready(function(){
+	var el = $('#apply-details');
+	if(el.length){
+		var str = $(el).text().split(" ");
+		var email;
+		var emailId;
+		$.each(str, function(index, item){
+			if(item.indexOf("@") > -1 && item.indexOf(".") > -1){
+				emailId = index;
+				email = $("<a>").attr("href", "mailto:" + item).text(item);
+			}	
+		});
+
+		console.log(email[0].outerHTML);
+		str[emailId] = email[0].outerHTML;
+
+		$(el).html(str.join(" "));
+	}
+});
