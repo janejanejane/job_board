715dab0c3fcf8ca81aa8a4d2179f553a2a0778b6
janejanejane <jeanclaudetteambait@gmail.com>
2013-04-05 12:49:25 +0800

    Change user show page to include all job preferences

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -2,8 +2,8 @@ module UsersHelper
 	def format_job_pref
 		@user_upvotes = Hash.new
 		CATEGORY.each_with_index do |choice, index| 
-			if(!@job_preference.empty?)
-				if(@job_preference.index{|d| d == index.to_s}) # checks if index is not nil
+			# if(!@job_preference.empty?) # limit display to job preference only
+				# if(@job_preference.index{|d| d == index.to_s}) # checks if index is not nil
 					vote_details = Vote.jobpref_vote(index, @user.id)
         	num = (@recorded_points.empty?) ? 0 : @recorded_points[index].to_i
 					if !vote_details.blank?
@@ -16,8 +16,8 @@ module UsersHelper
 					else
 						@user_upvotes[choice] = { points: "<b>[+#{num}]</b>", voters: "" }
 					end
-    		end
-  		end 
+    		# end
+  		# end 
  	 	end
 	end
 end
