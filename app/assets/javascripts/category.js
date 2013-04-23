12e27f924761f1cfc2e289f1ceb9e0e972458d6d
janejanejane <jeanclaudetteambait@gmail.com>
2013-04-23 21:32:44 +0800

    Add manifests to require page-specific js

---
+++ app/assets/javascripts/category.js
@@ -0,0 +1,3 @@
+// requires page-specific js files in user folder
+//= require user/infinite_scroll
+//= require user/jobpref_vote
---
+++ app/assets/javascripts/changepoints.js
@@ -0,0 +1,2 @@
+// requires page-specific js files in user folder
+//= require user/change_points
