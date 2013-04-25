971cff1080ce6c8a19c6218616fdb5981112dae6
janejanejane <jeanclaudetteambait@gmail.com>
2013-04-25 13:14:55 +0800

    Checking url: hash has no .length method

--- app/assets/javascripts/user/search_dev.js
+++ app/assets/javascripts/user/search_dev.js
@@ -3,18 +3,16 @@ $('document').ready(function(){
 
 	var parameters = getParams();
 
-	if(parameters.length > 0) {	
-		var search = parameters["search"];
-		var availability = parameters["availability"];
-		availability = (availability.indexOf("+") > -1) ? availability.replace("+", " ") : availability;
-		var location = parameters["location"];
+	var search = parameters["search"];
+	var availability = parameters["availability"];
+	availability = (availability && availability.indexOf("+") > -1) ? availability.replace("+", " ") : availability;
+	var location = parameters["location"];
 
-		console.log("search:", search, "availability:", availability, "location:", location);
+	console.log("search:", search, "availability:", availability, "location:", location);
 
-		$("#search").val(search);
-		$("#availability").val(availability);
-		$("#location").val(location);
-	}
+	$("#search").val(search);
+	$("#availability").val(availability);
+	$("#location").val(location);
 
 	function getParams(){
 		var searchStr = window.location.search.substring(1);
