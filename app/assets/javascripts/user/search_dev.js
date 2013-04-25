a5dda29e957e117844bfea07c8142bb6c1aaa6b2
janejanejane <jeanclaudetteambait@gmail.com>
2013-04-25 12:56:52 +0800

    Add checking if url has search parameters

--- app/assets/javascripts/user/search_dev.js
+++ app/assets/javascripts/user/search_dev.js
@@ -2,16 +2,19 @@ $('document').ready(function(){
 	console.log("search_dev loaded");
 
 	var parameters = getParams();
-	var search = parameters["search"];
-	var availability = parameters["availability"];
-	availability = (availability.indexOf("+") > -1) ? availability.replace("+", " ") : availability;
-	var location = parameters["location"];
 
-	console.log("search:", search, "availability:", availability, "location:", location);
+	if(parameters.length > 0) {	
+		var search = parameters["search"];
+		var availability = parameters["availability"];
+		availability = (availability.indexOf("+") > -1) ? availability.replace("+", " ") : availability;
+		var location = parameters["location"];
 
-	$("#search").val(search);
-	$("#availability").val(availability);
-	$("#location").val(location);
+		console.log("search:", search, "availability:", availability, "location:", location);
+
+		$("#search").val(search);
+		$("#availability").val(availability);
+		$("#location").val(location);
+	}
 
 	function getParams(){
 		var searchStr = window.location.search.substring(1);
